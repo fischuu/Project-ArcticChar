fd429026b432c8b6b5edbf978e764d87f7b7ce1f
fischuu <github@danielfischer.name>
2021-01-26 14:55:08 +0200

    barcodeid and sample file creation adjusted

--- runGBSPipeline-ArcticChar.sh
+++ runGBSPipeline-ArcticChar.sh
@@ -7,14 +7,13 @@
 #     FORMAT:   INDEX \ tab SampleName \tab BOOL, include in mock reference?
 # Remove all unnecessary lines from samplesheet.csv and then rrun:
 
-#grep -i "Artic_charr" /scratch/project_2001881/201204_NB551722_0018_AH2HWGBGXF/SampleSheet.csv | \
-#awk 'BEGIN {FS=","; OFS="\t"} {print $4"+"$6, $1 ,"NO"}' > /scratch/project_2003491/ArcticChar/barcodesID_Geno18.txt
+grep -i "Artic_charr" /scratch/project_2001881/201204_NB551722_0018_AH2HWGBGXF/SampleSheet.csv | \
+awk 'BEGIN {FS=","; OFS="\t"} {print $4"+"$6, $1 ,"NO"}' > /scratch/project_2003491/ArcticChar/barcodesID_Geno18.txt
 
-#grep -i "Arctic_charr" /scratch/project_2001881/201214_NB551722_0019_AHYGCTBGXC/SampleSheet.csv | \
-#awk 'BEGIN {FS=","; OFS="\t"} {print $8"+"$6, $1 ,"NO"}' | tail -n +2 > /scratch/project_2003491/ArcticChar/barcodesID_Geno19.txt
+grep -i "Arctic_charr" /scratch/project_2001881/201214_NB551722_0019_AHYGCTBGXC/SampleSheet.csv | \
+awk 'BEGIN {FS=","; OFS="\t"} {print $8"+"$6, $1 ,"NO"}' | tail -n +2 | tr -d ' ' > /scratch/project_2003491/ArcticChar/barcodesID_Geno19.txt
 
-#cat /scratch/project_2003491/ArcticChar/barcodesID_Geno18.txt /scratch/project_2003491/ArcticChar/barcodesID_Geno19.txt > /scratch/project_2003491/ArcticChar/barcodesID.txt
-#cut -f2 /scratch/project_2003491/ArcticChar/barcodesID.txt > /scratch/project_2003491/ArcticChar/samples
+cat /scratch/project_2003491/ArcticChar/barcodesID_Geno18.txt /scratch/project_2003491/ArcticChar/barcodesID_Geno19.txt | sed '/CS/d' |  sed '/PstI/d' | sed '/LB/d' | awk '{print $1"\t"$2"_\t"$3}' > /scratch/project_2003491/ArcticChar/barcodesID.txt
 
 module load bioconda/3
 source activate /projappl/project_2001746/conda_envs/Genotype
@@ -29,6 +28,7 @@ snakemake -s /scratch/project_2003491/Pipeline-GBS/GBS-pipeline.smk \
           -j 350 \
           --use-conda \
           --use-singularity \
+          --singularity-args "-B /scratch:/scratch,/projappl:/projappl" \
           --configfile /scratch/project_2003491/ArcticChar/GBS-pipeline_config-ArcticChar.yaml \
           --latency-wait 60 \
           --cluster-config /scratch/project_2003491/ArcticChar/GBS-pipeline_puhti-config.yaml \
